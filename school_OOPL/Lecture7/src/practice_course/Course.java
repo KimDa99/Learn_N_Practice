6225cc888de53a12c57a4fc70602ae22539f0afd
KimDa99 <91198933+KimDa99@users.noreply.github.com>
2022-04-06 21:40:30 +0900

    Lec7에 Prctice1 추가

--- school_OOPL/Lecture7/src/practice_course/Course.java
+++ school_OOPL/Lecture7/src/practice_course/Course.java
@@ -1,10 +1,36 @@
 package practice_course;
 
 public class Course {
+	  private String courseName;
+	  private String[] students = new String[100];
+	  private int numberOfStudents;
+	    
+	  public Course(String courseName) {
+	    this.courseName = courseName;
+	  }
+	  
+	  public void addStudent(String student) {
+	    students[numberOfStudents] = student;
+	    numberOfStudents++;
+	  }
+	  
+	  public String[] getStudents() {
+	    return students;
+	  }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	  public int getNumberOfStudents() {
+	    return numberOfStudents;
+	  }  
 
-	}
-
+	  public String getCourseName() {
+	    return courseName;
+	  }  
+	  
+	  public void dropStudent(String student) {
+	    // Left as an exercise in Exercise 10.9
+	  }
+	  
+	  public void clear() {
+		  
+	  }
 }
